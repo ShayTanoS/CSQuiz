af19b5d7475e3bd8bcb4dc8c1b20a0513583a3e7
ShayTanoS <maks.shyatna2017gmail.com>
2024-03-10 19:48:42 +0200

    Додано макет списку, який буде видавати список гравців.

--- CSQuiz/CSQuiz/settings.py
+++ CSQuiz/CSQuiz/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    'quizapp/static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- CSQuiz/quizapp/urls.py
+++ CSQuiz/quizapp/urls.py
@@ -4,5 +4,5 @@ from .views import AddPlayerView, QuizView
 urlpatterns = [
     path('update/', TemplateView.as_view(template_name='staff/update_list.html'), name='update_list'),
     path('update/add_player/', AddPlayerView.as_view(), name='add_player'),
-    path('quiz', QuizView.as_view(), name='quiz_page')
+    path('quiz/', QuizView.as_view(), name='quiz_page')
 ]
