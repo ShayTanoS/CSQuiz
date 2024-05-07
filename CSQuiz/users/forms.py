67672ba5e2056713502c5b59db3d04b1f0b9bcad
ShayTanoS <maks.shyatna2017gmail.com>
2024-05-07 20:14:13 +0300

    Перероблено форму реєстрації. Візуальні зміни на сторінках реєстрації.

--- CSQuiz/users/forms.py
+++ CSQuiz/users/forms.py
@@ -3,11 +3,24 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 User = get_user_model()
+
+
 class UserCreationForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={"autofocus": True}))
     email = forms.EmailField(label='Email', widget=forms.EmailInput(), max_length=254)
-    class Meta(UserCreationForm.Meta):
+    password1 = forms.CharField(
+        label=("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}))
+    password2 = forms.CharField(
+        label=("Password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}))
+
+    class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ['username', 'email', 'password1']
+
 
 class EmailConfirmForm(forms.Form):
     code = forms.IntegerField()
